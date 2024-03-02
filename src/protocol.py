352c4243f7e7e8592dcdfd02dce6e8bad45ad93e
Pedro Pinto <pmap@ua.pt>
2024-03-02 13:23:09 +0000

    Message - create all message Types

--- src/protocol.py
+++ src/protocol.py
@@ -1,23 +1,44 @@
-"""Protocol for chat server - Computação Distribuida Assignment 1."""
+#!/usr/bin/env python3
+'''
+ # @ Author: Pedro Pinto (pmap@ua.pt)
+ # @ Create Time: 2024-02-26
+ # @ Description: Protocol for chat server 
+                  Computação Distribuida Assignment 1.
+ '''
+
 import json
 from datetime import datetime
 from socket import socket
 
-
 class Message:
     """Message Type."""
-
+    
+    def __init__(self, command: str):
+        self.command = command
     
 class JoinMessage(Message):
     """Message to join a chat channel."""
+
+    def __init__(self, channel: str):
+        super().__init__("join")
+        self.channel = channel
 
 
 class RegisterMessage(Message):
     """Message to register username in the server."""
 
+    def __init__(self, username: str):
+        super().__init__("register")
+        self.user = username
     
 class TextMessage(Message):
     """Message to chat with other clients."""
+
+    def __init__(self, message: str, channel: str):
+        super().__init__("message")
+        self.message = message
+        self.channel = channel
+        self.ts = int(round(datetime.now().timestamp())) # Integer timestamp of current datetime
 
 
 class CDProto:
@@ -26,21 +47,23 @@ class CDProto:
     @classmethod
     def register(cls, username: str) -> RegisterMessage:
         """Creates a RegisterMessage object."""
-        return RegisterMessage(None)
+        return RegisterMessage(username)
 
     @classmethod
     def join(cls, channel: str) -> JoinMessage:
         """Creates a JoinMessage object."""
-        return JoinMessage(None)
+        return JoinMessage(channel)
 
     @classmethod
     def message(cls, message:str , channel: str = None) -> TextMessage:
         """Creates a TextMessage object."""
+        return TextMessage(message, channel)
 
     @classmethod
     def send_msg(cls, connection: socket, msg: Message):
         """Sends through a connection a Message object."""
-        connection.send(msg)
+        # Algorithm to send by groups...
+        connection.send(b"Ola")
 
 
     @classmethod
