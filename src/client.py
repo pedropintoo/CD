a6a7ed15850e60a3a9b0b72d61f6ef32fa03f3c5
Pedro Pinto <pmap@ua.pt>
2024-03-02 11:35:49 +0000

    Client - start structure

--- src/client.py
+++ src/client.py
@@ -1,23 +1,40 @@
-"""CD Chat client program"""
+#!/usr/bin/env python3
+'''
+ # @ Author: Pedro Pinto (pmap@ua.pt)
+ # @ Create Time: 2024-02-26
+ # @ Description: CD Chat client program
+ '''
+
 import logging
 import sys
+import socket
 
 from .protocol import CDProto, CDProtoBadFormat
-
 logging.basicConfig(filename=f"{sys.argv[0]}.log", level=logging.DEBUG)
 
+SERVER = "127.0.0.1" # localhost
+PORT = 8888 # server port
 
 class Client:
     """Chat Client process."""
 
     def __init__(self, name: str = "Foo"):
         """Initializes chat client."""
-        pass
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self):
         """Connect to chat server and setup stdin flags."""
-        pass
+        self.s.connect((SERVER,PORT)) # connect to server (block until accepted)
+        self.loop()
+        # Miss stdin flags
 
     def loop(self):
         """Loop indefinetely."""
-        pass
+        while (True):
+            # send some data
+            str_send = input(">")
+            self.s.send(str_send.encode("utf-8"))
+            
+            # receive the response
+            data = self.s.recv(1024)
+            print(data)
